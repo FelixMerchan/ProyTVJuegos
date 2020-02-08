1a6c52df5bc913ca5f890104e4fa13b901b9a8ea
FelixMerchan <56200776+FelixMerchan@users.noreply.github.com>
2020-02-08 14:27:15 -0500

    dadfasd

--- src/Modelos/Factura.java
+++ src/Modelos/Factura.java
@@ -5,5 +5,10 @@ public class Factura {
 	public String cedula;
 	public String nombres;
 	public String direccion;
+	public String ciudad;
+	public String telefono;
+	public String celular;
+	public String numero;
+	public String cod_producto;
 
 }
